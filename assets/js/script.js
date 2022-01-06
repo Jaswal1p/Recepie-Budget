cf65aa2c16a5cd2856eefe55c6bc371cdece0c34
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-06 16:17:34 -0500

    appended dynamic li element to ul element & YEEEESSSSSgit add -A It worksgit add -A

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,8 @@ let createTaskHandler = function(event) {
 
     event.preventDefault();
 
-    let purchaseInput = document.querySelector("input[name='purchase']").ariaValueMax;
-    let purchaseTypeInput = document.querySelector("select[name='category']").ariaValueMax;
+    let purchaseInput = document.querySelector("input[name='purchase-name']").value;
+    let purchaseTypeInput = document.querySelector("select[name='category']").value;
 
     let purchaseMadeEl = document.createElement("li");
     purchaseMadeEl.className = "purchase-item";
@@ -18,6 +18,9 @@ let createTaskHandler = function(event) {
     purchaseInfoEl.className = "purchase-info";
 
     purchaseMadeEl.innerHTML = "<h3 class='purchase-name'>" + purchaseInput + "</h3><span class='category'>" + purchaseTypeInput + "</span>";
+
+    purchaseEl.appendChild(purchaseMadeEl);
+    console.log(purchaseEl);
 }
     
 
