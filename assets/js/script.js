ae58d98767c106acaea235166adebd0dda647252
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-06 16:08:06 -0500

    code added for dynamically created HTML for input and input type

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ console.log(formEl);
 let purchaseEl = document.querySelector("#purchase");
 console.log(purchaseEl);
 
-formEl.addEventListener("submit", createTaskHandler); 
 
 let createTaskHandler = function(event) {
 
@@ -12,7 +11,14 @@ let createTaskHandler = function(event) {
     let purchaseInput = document.querySelector("input[name='purchase']").ariaValueMax;
     let purchaseTypeInput = document.querySelector("select[name='category']").ariaValueMax;
 
-    
+    let purchaseMadeEl = document.createElement("li");
+    purchaseMadeEl.className = "purchase-item";
+
+    let purchaseInfoEl = document.createAttribute("div");
+    purchaseInfoEl.className = "purchase-info";
+
+    purchaseMadeEl.innerHTML = "<h3 class='purchase-name'>" + purchaseInput + "</h3><span class='category'>" + purchaseTypeInput + "</span>";
 }
     
 
+formEl.addEventListener("submit", createTaskHandler); 
