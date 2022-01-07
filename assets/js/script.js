cfb5af6c39fc9bb14bc60fd1bc85919a1b546e70
Kate Spitalnic <kespitalnic@gmail.com>
2022-01-07 00:45:33 -0500

    functioning intro screen. input clears and user is alerted if an invalid value for budget is inputted

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,37 @@ let purchaseEl = document.querySelector("#purchase");
 console.log(purchaseEl);
 var apiKey = "AIzaSyBsxqmtTs7-gPZXL68yoiN01dtF9hL5vfI"
 
-var section1 = document.querySelector(".enterBudget");
+var intro = document.querySelector(".enterBudget");
 var calendar = "https://www.googleapis.com/calendar/v3&appid=" + apiKey;
 
 
-console.log(calendar);
-fetch(calendar)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data);
-    })
-// $("#submitBtn").click(function(){
-//     console.log("click");
-//     var budget = document.getElementById("setLimit").value;
-//     if (isNaN(budget)) {
-//         window.alert("Please enter a numeric value.");
-//         $("#setLimit").value=" ";
-//         return;
-//       }
-//       else {
-//         console.log(budget);
-//         localStorage.setItem("initialBudget", budget);
-//         section1.setAttribute("class", "hide")
-//       }
-// })
+// console.log(calendar);
+// fetch(calendar)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function(data){
+//         console.log(data);
+//     })
+
+document.querySelector(".application").style.display="none"
+
+$("#submitBtn").click(function(){
+    console.log("click");
+    var budget = document.getElementById("setLimit").value;
+    console.log(budget)
+    if (isNaN(budget)) {
+        document.getElementById("setLimit").value="";
+        window.alert("Please enter a numeric value.");
+        return;
+      }
+      else {
+        console.log(budget);
+        localStorage.setItem("initialBudget", budget);
+        intro.setAttribute("class", "hide");
+        document.querySelector(".application").style.display="block"
+    }
+})
 
 let createTaskHandler = function(event) {
 
